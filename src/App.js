35ae44cb3c2d9b6fa17ecca9ca9eb425c080d119
shidalgra <shidalgra@gmail.com>
2025-01-07 10:12:18 -0600

    Subiendo prop(propiedades) y validaciones de props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import PropTypes from "prop-types" // sirve para validar props
 
 function App() {
+
+  const usuario = {
+    nombre: "Sergio",
+    apellido: "Hidalgo",
+    edad: 38,
+    genero: "masculino"
+  }
+
+  const acompañante = {
+    nombre: "Karen",
+    apellido: "Chinchilla", 
+    edad: 36,
+    genero: "femenino"
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Saludo destinatario=" Un saludo a: " usuario={usuario} acompañante={acompañante}></Saludo>
     </div>
   );
+}
+
+function Saludo(props) {
+  return (
+    <div>
+      <h1>Hola {props.destinatario} {props.usuario.nombre} {props.usuario.apellido} !!!</h1>
+      <h2>y tambien {props.destinatario} {props.acompañante.nombre} {props.acompañante.apellido}</h2>
+    </div>
+  )
+}
+
+// para validar propiedades estas casi siempre se hacen despues de lo que queremos validar
+Saludo.propTypes = {
+  usuario:PropTypes.shape(       //queremos que el nombre sea requerido
+    {
+      nombre:PropTypes.string.isRequired,  //esto no saldra para el usuario pero si quitamos nombre en el codigo de arriba, saldra un error en la pagina cuando vamos a consola de chrome
+      edad:PropTypes.number.isRequired
+    }
+  ).isRequired
 }
 
 export default App;
