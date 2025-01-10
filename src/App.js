ff30f4005dec7ad919deadb38a661f59b1005f99
shidalgra <shidalgra@gmail.com>
2025-01-09 20:10:13 -0600

    Cambios en el .Json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import './App.css';
-import PropTypes from "prop-types" // sirve para validar props
+import PropTypes from "prop-types"; // sirve para validar props
 
 function App() {
 
   const usuario = {
-    nombre: "Sergio",
     apellido: "Hidalgo",
-    edad: 38,
-    genero: "masculino"
+    edad: "38",
+    genero: "Masculino"
   }
-
-  const acompañante = {
-    nombre: "Karen",
-    apellido: "Chinchilla", 
-    edad: 36,
-    genero: "femenino"
-  }
-
 
   return (
     <div>
-      <Saludo destinatario=" Un saludo a: " usuario={usuario} acompañante={acompañante}></Saludo>
+      <Saludo destinatario=" Un saludo a: " usuario={usuario}></Saludo>
     </div>
   );
 }
@@ -28,18 +19,18 @@ function App() {
 function Saludo(props) {
   return (
     <div>
-      <h1>Hola {props.destinatario} {props.usuario.nombre} {props.usuario.apellido} !!!</h1>
-      <h2>y tambien {props.destinatario} {props.acompañante.nombre} {props.acompañante.apellido}</h2>
+      <h1>Hola {props.destinatario} {props.usuario.apellido}!!!</h1>
+      <h2>edad: {props.usuario.edad}</h2>
+      <h2>Genero: {props.usuario.genero}</h2>
     </div>
-  )
+  );
 }
 
-// para validar propiedades estas casi siempre se hacen despues de lo que queremos validar
 Saludo.propTypes = {
-  usuario:PropTypes.shape(       //queremos que el nombre sea requerido
+  usuario: PropTypes.shape(    
     {
-      nombre:PropTypes.string.isRequired,  //esto no saldra para el usuario pero si quitamos nombre en el codigo de arriba, saldra un error en la pagina cuando vamos a consola de chrome
-      edad:PropTypes.number.isRequired
+      nombre: PropTypes.string.isRequired,  
+      edad: PropTypes.number.isRequired
     }
   ).isRequired
 }
